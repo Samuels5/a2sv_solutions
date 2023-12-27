926582763d4c9ab10acc56c2259b7bd1069d40bc
Samuels5 <116708327+Samuels5@users.noreply.github.com>
2023-12-27 13:57:19 -0300

    Added solution for Intersection of Two Arrays II

---
+++ leetcode/intersection-of-two-arrays-ii.py
@@ -0,0 +1,9 @@
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        c=Counter(nums1)
+        d=Counter(nums2)
+        a=[]
+        for num in d.keys():
+            if num in c:
+                a.extend([num]*min((c[num],d[num])))
+        return a
