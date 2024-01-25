971ef67bf9675fa7d4315467843497e59896bde0
Samuels5 <116708327+Samuels5@users.noreply.github.com>
2024-01-25 12:15:12 -0300

    Added solution for Design Linked List

--- leetcode/design-linked-list.py
+++ leetcode/design-linked-list.py
@@ -1,54 +1,78 @@
 class Node:
-    def __init__(self,val,nextNode=None):
+    def __init__(self,val):
         self.val=val 
-        self.next = nextNode       
+        self.next = None       
 
 class MyLinkedList:
 
     def __init__(self):
-        self.head = Node(0)
+        self.head = None
         self.size = 0
         
 
     def get(self, index: int) -> int:
-        if index >= self.size: 
-            return -1
-
-        cur = self.head
-
-        for _ in range(index + 1):
-            cur = cur.next
-        return cur.val
+        if index < self.size and self.head :
+            cur = self.head
+            for i in range(index):
+                cur=cur.next
+            print(self.size)
+            return cur.val
+        print(-1)
+        return -1
 
     def addAtHead(self, val: int) -> None:
-        self.addAtIndex(0, val)
+        temp = Node(val)
+        if self.head == None:
+            self.head=temp
+        else:
+            temp.next=self.head
+            self.head = temp
+        self.size+=1
         
 
     def addAtTail(self, val: int) -> None:
-        self.addAtIndex(self.size, val)
+        temp = Node(val)
+        if self.head == None:
+            self.head = temp
+        else:
+            cur = self.head
+            while cur.next != None:
+                cur=cur.next
+            cur.next=temp
+        self.size+=1
         
 
     def addAtIndex(self, index: int, val: int) -> None:
-        if index > self.size: 
-            return -1
-
-        prev = self.head
-        for _ in range(index):
-            prev = prev.next
-        newNode = Node(val, prev.next)
-        prev.next = newNode
-        self.size += 1
+        
+        if index <= self.size:
+            if index == 0:
+                self.addAtHead(val)
+            elif index == self.size:
+                self.addAtTail(val)
+            else:
+                temp = Node(val)
+                cur = self.head
+                for i in range(index-1):
+                    cur=cur.next
+                temp.next =cur.next
+                cur.next = temp
+                self.size+=1
                     
+            
+
+
+        
+
     def deleteAtIndex(self, index: int) -> None:
-        if index >= self.size: 
-            return -1
-
-        prev = self.head
-        for _ in range(index):
-            prev = prev.next
-
-        prev.next = prev.next.next
-        self.size -= 1
+        cur = self.head
+        if index < self.size:
+            if index ==  0:
+                self.head = cur.next
+            else:
+                for i in range(index-1):
+                    cur=cur.next
+                cur.next =cur.next.next
+            self.size -= 1  
 
         
 
