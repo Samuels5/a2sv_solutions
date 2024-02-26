61ff1ee741db879042764272f0b528801c6a93ce
Samuels5 <116708327+Samuels5@users.noreply.github.com>
2024-02-26 13:51:32 -0300

    Added solution for Count Good Numbers

---
+++ leetcode/count-good-numbers.py
@@ -0,0 +1,15 @@
+class Solution:
+    def __init__(self):
+        self.M = 1000_000_007
+    def countGoodNumbers(self, n: int) -> int:
+        def myPow(x, m):
+            if m==0:
+                return 1
+    
+            half=myPow(x,m//2)
+            if m%2==0:
+                return (half*half)%(self.M)
+            return (half*half*x)%self.M
+        even=(n+1)//2
+        odd=n//2
+        return (myPow(5,even)*myPow(4,odd))%self.M
