ebbfa9120efba5653a8828fcef6655c8dfbc8a12
Samuels5 <116708327+Samuels5@users.noreply.github.com>
2024-07-01 22:00:27 +0300

    Created 'Magnetic Force Between Two Balls' by A2SV Hub

---
+++ 01-Jul-2024/Magnetic Force Between Two Balls 175335.py
@@ -0,0 +1,26 @@
+# Problem: Magnetic Force Between Two Balls - https://leetcode.com/problems/magnetic-force-between-two-balls/
+
+class Solution:
+    def maxDistance(self, position: List[int], m: int) -> int:
+        position.sort()
+        
+        def helper(num):
+            count = 0
+            i = 0
+            while i < len(position):
+                i = bisect_left(position, num + position[i])
+                count += 1
+                if count >= m:
+                    return True
+            return False
+
+        left = -1
+        right = 10**9
+        while left + 1 < right:
+            mid = (left + right)//2
+            if helper(mid):
+                left = mid
+            else:
+                right = mid
+
+        return left
