2a7fbacb996b5069fd3458464cf7315404c153af
Samuels5 <116708327+Samuels5@users.noreply.github.com>
2024-07-17 21:08:55 +0300

    Created 'Diagonal Traverse' by A2SV Hub

---
+++ 17-Jul-2024/Diagonal Traverse 181271.py
@@ -0,0 +1,22 @@
+# Problem: Diagonal Traverse - https://leetcode.com/problems/diagonal-traverse/
+
+class Solution:
+    def findDiagonalOrder(self, mat: List[List[int]]) -> List[int]:
+        d=defaultdict(list)
+        
+        for raw in range(len(mat)):
+            for col in range(len(mat[0])):
+                d[raw+col].append(mat[raw][col])
+
+        flag=1
+        re=[]
+
+        for val in d.values():
+            if flag==1:
+                re+=val[::-1]
+                flag=0
+            else:
+                re+=val
+                flag=1
+                
+        return re
