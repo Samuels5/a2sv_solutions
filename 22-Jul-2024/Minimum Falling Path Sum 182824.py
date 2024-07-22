95578589dd825d2d458faf15de577f13de0248ca
Samuels5 <116708327+Samuels5@users.noreply.github.com>
2024-07-22 20:47:55 +0300

    Created 'Minimum Falling Path Sum' by A2SV Hub

---
+++ 22-Jul-2024/Minimum Falling Path Sum 182824.py
@@ -0,0 +1,33 @@
+# Problem: Minimum Falling Path Sum - https://leetcode.com/problems/minimum-falling-path-sum/
+
+class Solution:
+    def minFallingPathSum(self, matrix: List[List[int]]) -> int:
+        direction = [(1,-1),(1,0),(1,1)]
+        sumi = 0
+        num = inf
+        memo = {}
+
+        def inbound(r, c):
+            return 0 <= r < len(matrix) and 0 <= c <len(matrix[0])
+        
+        def dp(row, col):
+            state = (row, col)
+            if state not in memo:
+                if row == len(matrix) - 1:
+                    return 0
+                    
+                val = inf
+
+                for r, c in direction:
+                    nr, nc = row + r, col + c
+                    if inbound(nr, nc):
+                        val = min(dp(nr, nc) + matrix[nr][nc], val)
+                        
+                memo[state] = val
+
+            return memo[state]
+
+        for i in range(len(matrix[0])):
+            num = min(dp(-1,i), num)
+
+        return num
