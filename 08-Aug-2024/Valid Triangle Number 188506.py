cef6de3b3ae013537d25d15b4516c31d6815cab7
Samuel Mulugeta <116708327+Samuels5@users.noreply.github.com>
2024-08-08 21:58:40 +0300

    Created 'Valid Triangle Number' by A2SV Hub

---
+++ 08-Aug-2024/Valid Triangle Number 188506.py
@@ -0,0 +1,20 @@
+# Problem: Valid Triangle Number - https://leetcode.com/problems/valid-triangle-number/
+
+class Solution:
+    def triangleNumber(self, nums: List[int]) -> int:
+        nums.sort(reverse = True)
+        count = 0
+        n = len(nums)
+        for i in range(n - 2):
+            left = i + 1
+            right = n-1
+            while left < right:
+                summ = nums[left] + nums[right]
+
+                if summ > nums[i]:
+                    count += right - left
+                    left += 1
+                else:
+                    right -= 1
+                    
+        return count
