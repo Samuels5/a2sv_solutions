6d4577628814cc9cedcbc117fa3d3872fde45bc7
Samuel Mulugeta <116708327+Samuels5@users.noreply.github.com>
2024-08-22 23:44:45 +0300

    Created 'F - Post-war Games' by A2SV Hub

---
+++ A2SV G5 - Contest #9 | Adwa Special! 22-Aug-2024/F - Post-war Games 198832.py
@@ -0,0 +1,17 @@
+# Problem: F - Post-war Games - https://codeforces.com/gym/513152/problem/F
+
+from sys import stdin
+
+
+def input(): return stdin.readline().strip()
+
+T = int(input())
+for _ in range(T):
+    N, K = map(int, input().split())
+
+    if N == K:
+        print(0)
+        continue
+
+    ans = (N - (K + 1))*3 + ((K - 1)//2)*3 + (K - 1)%2 + 1
+    print(ans)
