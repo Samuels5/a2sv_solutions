34e1d6e0a4d6057c2edbc8dc95cdd7e70dbe97d7
Samuel Mulugeta <116708327+Samuels5@users.noreply.github.com>
2024-09-10 21:38:35 +0300

    Created 'Find and Replace Pattern' by A2SV Hub

---
+++ 10-Sep-2024/Find and Replace Pattern 214161.py
@@ -0,0 +1,26 @@
+# Problem: Find and Replace Pattern - https://leetcode.com/problems/find-and-replace-pattern/
+
+class Solution:
+    def findAndReplacePattern(self, words: List[str], pattern: str) -> List[str]:
+        ans = []
+        for word in words:
+            pat = defaultdict(str)
+            d = defaultdict(str)
+            flag = 0
+            for idx, val in enumerate(word):
+                if val in pat :
+                    if not pat[val] == pattern[idx] :
+                        flag = 1
+                        break
+                elif pattern[idx] in d:
+                    if not d[pattern[idx]] == val :
+                        flag = 1
+                        break
+                else:
+                    pat[val] = pattern[idx]
+                    d[pattern[idx]] = val
+
+            if not flag:
+                ans.append(word)
+
+        return ans
