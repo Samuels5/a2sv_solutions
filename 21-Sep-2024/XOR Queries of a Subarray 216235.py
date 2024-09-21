14bdeb4eab754257b57a43306e91218bc8c71863
Samuel Mulugeta <116708327+Samuels5@users.noreply.github.com>
2024-09-21 22:10:39 +0300

    Created 'XOR Queries of a Subarray' by A2SV Hub

---
+++ 21-Sep-2024/XOR Queries of a Subarray 216235.py
@@ -0,0 +1,13 @@
+# Problem: XOR Queries of a Subarray - https://leetcode.com/problems/xor-queries-of-a-subarray/
+
+class Solution:
+    def xorQueries(self, arr: List[int], queries: List[List[int]]) -> List[int]:
+        pre = [0]
+        for val in arr:
+            pre.append(pre[-1]^val)
+            
+        ans = []
+        for val in queries:
+            ans.append(pre[val[0]]^pre[val[1]+1])
+
+        return ans
